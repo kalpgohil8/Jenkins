1ee3dcfc04893fb9b1b32014f21e317a802709e3
kalpgohil8 <kalpgohil9@gmail.com>
2023-12-15 10:36:02 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -33,11 +33,13 @@ node {
         def input_after = readJSON file: "${workdir()}/${project()}/input.json"
         echo "${input_after}"
 
-        script {
-            sh "python3 get_dynamic_ip.py ${workdir()}/${project()}/input.json B > SYNC_A_IP.log"
-            def AIP = readFile('SYNC_A_IP.log').trim()
-            echo "AIP: ${AIP}"
-        } 
+
+        sh '''
+            python3 get_dynamic_ip.py ${workdir()}/${project()}/input.json B > SYNC_A_IP.log"
+            AIP = `cat SYNC_A_IP.log`
+            echo $AIP
+        '''
+
     }
 }
 
