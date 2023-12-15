4c9726646a09d8dcfd271808680e8e1eaa8aec4c
kalpgohil8 <kalpgohil9@gmail.com>
2023-12-15 11:11:42 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -34,7 +34,7 @@ node {
 
         sh '''
             python3 get_dynamic_ip.py ${workdir()}/${project()}/input.json B > SYNC_A_IP.log
-            def AIP = readFile("SYNC_A_IP.log").trim()
+            AIP = "cat SYNC_A_IP.log"
             println("AIP: ${AIP}")
         '''
     }
