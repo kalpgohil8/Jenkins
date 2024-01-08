7c2129301af7be51934345986536b2d501c649fd
kalpgohil8 <kalpgohil9@gmail.com>
2024-01-08 23:43:35 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -12,20 +12,15 @@ node {
             }
         }
 
-        script{
-            sh """
-             ls
-             pwd
-            """
-        }
-
         def jsonFile = readJSON file: "Json"
         echo "${jsonFile}"
 
-        // script {
-        //     sh "python3 get_dynamic_ip.py ${workdir()}/${project()}/input.json B > SYNC_A_IP.log"
-        //     def AIP = readFile('SYNC_A_IP.log').trim()
-        //     echo "AIP: ${AIP}"
-        // } 
+        script {
+            sh "python kalp.py"
+        } 
+
+        def jsonFile1 = readJSON file: "Json"
+        echo "${jsonFile1}"
+
     }
 }
