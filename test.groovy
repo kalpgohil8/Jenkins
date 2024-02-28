6ecf1556ada415eccb463c11637e1f93789e9a4c
Kalp Gohil <kalpgohil9@gmail.com>
2024-02-28 13:49:42 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -11,6 +11,7 @@ node {
         def run_mode_val_array = iteration['run_mode'].split("\\|")
         run_mode_val_array = run_mode_val_array.collect{ mod_val ->mod_val.trim() }
         println run_mode
+        println run_mode_val_array.join(', ')
         if(run_mode_val_array.contains(run_mode)){
             println "DONE"
         } else {
