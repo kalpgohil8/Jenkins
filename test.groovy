f77c273e3ba64518df278fb26e774f51f040dc5c
Kalp Gohil <kalpgohil9@gmail.com>
2024-02-28 13:43:41 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -8,8 +8,8 @@ node {
         def json_data = readJSON file: "data.json"
         json_data.each { iteration ->
         println iteration
-        def run_mode_val_array = iteration['run_mode'].split("|")
-        println run_mode_val_array
+        def run_mode_val_array = iteration['run_mode'].split("\\|")
+        echo run_mode_val_array
         }
     }
 }
