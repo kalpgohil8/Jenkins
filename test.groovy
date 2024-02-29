29595b38e8f793d3e610b7ba91867fb54f0235e3
Kalp Gohil <kalpgohil9@gmail.com>
2024-02-29 16:59:06 +0530

    Done

--- test.groovy
+++ test.groovy
@@ -7,14 +7,22 @@ node {
     stage('Test') {
         def json_data = readJSON file: "data.json"
         json_data.each { iteration ->
-        println iteration
-        def run_mode_val_array = iteration['run_mode'].split("\\|")
-        run_mode_val_array = run_mode_val_array.collect{ mod_val ->mod_val.trim() }
-        if(run_mode_val_array.contains(run_mode.trim())){
-            println "DONE"
-        } else {
-            println "NOT DONE"
-        }
+            println iteration
+            
+            def run_mode_val_array = iteration['run_mode'].split("\\|")
+            run_mode_val_array = run_mode_val_array.collect{ mod_val ->mod_val }
+            
+            if(run_mode_val_array.contains(run_mode)){
+                def version_val_array = iteration['version'].split("\\|")
+                version_val_array = version_val_array.collect{ mod_val ->mod_val }
+                
+                if (run_mode_val_array.size() == version_val_array.size()){
+                    println "DONE"
+                }
+                else {
+                    println "NOT DONE"
+                }
+            }
         }
     }
 }
